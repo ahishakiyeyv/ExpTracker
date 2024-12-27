d414228bb428199c82ea6d5d6bdd54eabda14f28
ahishakiyeyv <yvanahishakiye6@gmail.com>
2024-12-27 02:46:01 +0100

    bar chart added

--- lib/screens/stats/stats.dart
+++ lib/screens/stats/stats.dart
@@ -1,3 +1,5 @@
+import 'package:exptracker/screens/stats/chart.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class StatScreen extends StatelessWidget {
@@ -12,12 +14,28 @@ class StatScreen extends StatelessWidget {
           vertical: 10.0,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               "Transactions",
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(12, 20, 12, 20),
+                child: const MyChart(),
               ),
             ),
           ],
