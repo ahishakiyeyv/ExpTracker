9fe9577b7782b672d3022150dd996a86a6eef1e4
ahishakiyeyv <yvanahishakiye6@gmail.com>
2024-12-31 13:42:36 +0100

    adding bloc for the backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:bloc/bloc.dart';
 import 'package:exptracker/app.dart';
+import 'package:exptracker/simple_block_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlockObserver();
   runApp(const MyApp());
 }
